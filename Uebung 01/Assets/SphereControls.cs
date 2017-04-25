d665e1915fc083be20d64037734c0c455ca3b9d8
Markus Hoehn <mahn@mahndo.local>
2017-04-25 12:39:33 +0200

    erster test

---
+++ Uebung 01/Assets/SphereControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SphereControls : MonoBehaviour {
+
+    public Text caption;
+
+	// Use this for initialization
+	void Start () {
+        caption.text = "This is ScreenText using YSIWYG-UI System";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0.3f, 0));
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 delta = new Vector3(0, -0.3f, 0);
+            transform.position += delta;
+        }
+	}
+}
